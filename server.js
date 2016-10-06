3eefc1c809df02f89ca1b774de5e5e88541e770d
rajum3000 <rajum3000@gmail.com>
2016-10-06 17:08:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function (req, res) {
-    var articleName = req.parms.articleName;
+    var articleName = req.params.articleName;
     res.send(createtemplate(articles[articleName]));
 });
 
