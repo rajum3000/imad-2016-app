1b67f012285fbde61a906527661d2afac9381198
rajum3000 <rajum3000@gmail.com>
2016-10-06 17:01:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
+var articles = {
+    'article-one' : {
     title: 'Article one | Raju M',
     heading: "Article One",
     time: "Sep 5, 2016",
@@ -20,7 +21,39 @@ var articleOne = {
                  This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
         </p>
             `
-    
+    }, 
+    'article-two' : {
+        title: 'Article Two | Raju M',
+        heading: "Article Two",
+        time: "Sep 5, 2016",
+        content: `
+        <p>
+                This is the content for my second article.This is the content for my second article.This is the content for my second article. 
+        </p>
+        <p>
+                 This is the content for my second article.This is the content for my second article.This is the content for my second article.
+        </p>
+        <p>
+                 This is the content for my second article.This is the content for my second article.This is the content for my second article.
+        </p>
+            `
+    },
+    'article-three' : {
+        title: 'Article Three | Raju M',
+        heading: "Article Three",
+        time: "Sep 5, 2016",
+        content: `
+        <p>
+                This is the content for my third article.This is the content for my third article.This is the content for my third article. 
+        </p>
+        <p>
+                 This is the content for my third article.This is the content for my third article.This is the content for my third article.
+        </p>
+        <p>
+                 This is the content for my third article.This is the content for my third article.This is the content for my third article.
+        </p>
+            `
+    },
 };
 
 function createtemplate(data)
@@ -65,16 +98,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(createtemplate(articleOne));
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articlename', function (req, res) {
+    var atriclename = req.parms.articlename;
+  res.send(createtemplate(articles[articlename]));
 });
 
 app.get('/ui/style.css', function (req, res) {
