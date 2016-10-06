3a57580e8a9e6dac5ee4178852c46aa7fa53630f
rajum3000 <rajum3000@gmail.com>
2016-10-06 18:30:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 // change the text
-var element = document.getElementById('main-text');
+var element = document.getElementById("main-text");
 element.innerHtml = 'new value';
